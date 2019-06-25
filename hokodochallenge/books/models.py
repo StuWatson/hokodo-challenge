e5898ecf7fe8e888fa235eba69aa0fbd6937b2ce
Stu Watson <stuart.watson@appsbroker.com>
2019-06-25 17:31:26 +0100

    Adding book model

--- hokodochallenge/books/models.py
+++ hokodochallenge/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.CharField(primary_key=True, max_length=36)
+    cover = models.URLField()
+    isbn = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    published = models.DateTimeField()
+    publisher = models.CharField(max_length=255)
+    pages = models.IntegerField()
+    description = models.TextField()
+    website = models.URLField()
+    author = models.CharField(max_length=255)
